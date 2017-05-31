b5ffd6d9b8fce157ed453e86582773817b67e756
Yoshihiro Kameda <kameda.sbng@gmail.com>
2017-06-01 00:48:44 +0900

    implement create PR code.

---
+++ scripts/create_pull_request.rb
@@ -0,0 +1,11 @@
+require 'octokit'
+
+client = Octokit::Client.new(access_token: ENV['GITHUB_ACCESS_TOKEN'])
+client.create_pull_request(
+  'user-name/repo-name',  # 適当に変える
+  'master',
+  ENV['BRANCH'],
+  'Zip data updated',  # Title
+  ''                # Body
+)
+
