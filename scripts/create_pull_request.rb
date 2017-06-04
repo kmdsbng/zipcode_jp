69ae2efd366460dafba8c5d0de6b3a6659a6c36f
Yoshihiro Kameda <kameda.sbng@gmail.com>
2017-06-04 16:38:51 +0900

    add debug code.

--- scripts/create_pull_request.rb
+++ scripts/create_pull_request.rb
@@ -1,5 +1,8 @@
 require 'octokit'
 
+puts "BRANCH: #{ENV['BRANCH']}"
+puts "GITHUB_ACCESS_TOKEN: #{ENV['GITHUB_ACCESS_TOKEN']}"
+
 client = Octokit::Client.new(access_token: ENV['GITHUB_ACCESS_TOKEN'])
 client.create_pull_request(
   'kmdsbng/zipcode_jp',  # 適当に変える
